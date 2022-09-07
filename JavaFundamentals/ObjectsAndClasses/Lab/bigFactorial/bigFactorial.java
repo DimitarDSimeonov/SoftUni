1f671b14f8299a9228099d7e745d819e09e8a1c1
Dimitar Simeonov <112191155+DimitarDSimeonov@users.noreply.github.com>
2022-09-07 18:47:48 +0300

    Create bigFactorial.java

---
+++ JavaFundamentals/ObjectsAndClasses/Lab/bigFactorial/bigFactorial.java
@@ -0,0 +1,19 @@
+package bigFactorial;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigFactorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        BigInteger sum = new BigInteger("1");
+
+        for (int i = 1; i <= n; i++) {
+            sum = sum.multiply(BigInteger.valueOf(i));
+        }
+
+        System.out.println(sum);
+    }
+}
