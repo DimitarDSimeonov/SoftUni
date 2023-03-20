6eeeaba0c96e56b4b1353f7b9da3a0a760f51856
Dimitar Simeonov <simeonov88@abv.bg>
2023-03-20 22:01:09 +0200

    Update SumTable.js

--- JS Front End/DOM and Events/Lab/08. Sum-Table/SumTable.js
+++ JS Front End/DOM and Events/Lab/08. Sum-Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+    const pricesArr = Array.from(document.querySelectorAll('td:nth-child(even)'));
+    let totalSum = 0;
+    for (let i = 0; i < pricesArr.length - 1; i++) {
+        totalSum += Number(pricesArr[i].textContent);
+    }
 
+    let sum = document.getElementById('sum');
+    sum.textContent = totalSum;
 }
