043eb2aa54c0eb566fe595abb95d185a671785fe
Dimitar Simeonov <simeonov88@abv.bg>
2023-03-22 21:11:40 +0200

    Update app.js

--- JS Front End/DOM and Events/Lab/09. Highlight-Active/app.js
+++ JS Front End/DOM and Events/Lab/09. Highlight-Active/app.js
@@ -1,3 +1,21 @@
 function focused() {
-    console.log('TODO:...');
+    const inputArr = Array.from(document.getElementsByTagName('input'));
+
+    for (const input of inputArr) {
+        input.addEventListener('focus', focus);
+        input.addEventListener('blur', blur);
+    }
+
+
+    function focus(event) {
+        const currentInput = event.target;
+        const parentDiv = currentInput.parentElement;
+        parentDiv.classList.add('focused');
+    }
+
+    function blur(event) {
+        const currentInput = event.target;
+        const parentDiv = currentInput.parentElement;
+        parentDiv.classList.remove('focused');
+    }
 }
