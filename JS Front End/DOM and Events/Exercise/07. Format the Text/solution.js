3aca487654e2d7cc306b199d317ad47e34e23d1a
Dimitar Simeonov <simeonov88@abv.bg>
2023-03-26 21:34:57 +0300

    Update solution.js

--- JS Front End/DOM and Events/Exercise/07. Format the Text/solution.js
+++ JS Front End/DOM and Events/Exercise/07. Format the Text/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input');
+  let outputDiv = document.getElementById('output');
+
+  let text = input.value.split('.');
+  text.pop();
+
+  while(text.length > 0) {
+  let paragraphText = text.splice(0, 3).map(e => e.trimStart());
+  let p = document.createElement('p');
+  p.textContent = paragraphText.join('.') + '.';
+  outputDiv.appendChild(p);
+  }
 }
