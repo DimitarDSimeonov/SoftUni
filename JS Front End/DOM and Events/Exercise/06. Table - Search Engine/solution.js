800907f552fc44cdff19698e7c896d97e8c756e4
Dimitar Simeonov <simeonov88@abv.bg>
2023-03-26 20:45:31 +0300

    Update solution.js

--- JS Front End/DOM and Events/Exercise/06. Table - Search Engine/solution.js
+++ JS Front End/DOM and Events/Exercise/06. Table - Search Engine/solution.js
@@ -2,7 +2,23 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let allTd = Array.from(document.querySelectorAll('tbody td'));
+      let input = document.getElementById('searchField');
+      let searchingText = input.value;
 
+      for (const td of allTd) {
+         if (td.textContent.includes(searchingText)){
+            let tr = td.parentElement;
+            tr.classList.add('select');
+         }
+      }
+      input.value = '';
+
+      input.addEventListener('focus', () =>{
+         for (const td of allTd) {
+            let tr = td.parentElement;
+            tr.classList.remove('select');
+         }
+      });
    }
 }
