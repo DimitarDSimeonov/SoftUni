37d2a7f92c7b4e8e1e72fecc64d0a077cd9f2746
Dimitar Simeonov <simeonov88@abv.bg>
2023-04-01 22:36:06 +0300

    Update app.js

--- JS Front End/HTTP and REST/Lab/06. Github-Commits/app.js
+++ JS Front End/HTTP and REST/Lab/06. Github-Commits/app.js
@@ -1,4 +1,24 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let BASE_URL = 'https://api.github.com/repos/';
+    let inputUsername = document.getElementById('username');
+    let username = inputUsername.value;
+    let inputRepo = document.getElementById('repo');
+    let repo = inputRepo.value;
+    let ul = document.getElementById('commits');
+    ul.innerHTML = ''
+
+    fetch(`${BASE_URL}${username}/${repo}/commits`)
+    .then((res) => res.json())
+    .then((data) => {
+        data.forEach(element => {
+            let li = document.createElement('li');
+            li.textContent = `${element.commit.author.name}: ${element.commit.message}`
+            ul.appendChild(li);
+        });
+    })
+    .catch((err) => {
+        let li = document.createElement('li');
+        li.textContent = `Error: 404 (Not Found)`;
+        ul.appendChild(li);
+    });   
 }
