bb35c69fdbf377f0f8d412ace58e5cfbaabf716e
Dimitar Simeonov <simeonov88@abv.bg>
2023-04-01 18:07:36 +0300

    Update app.js

--- JS Front End/HTTP and REST/Lab/04. GitHub-Repos/scripts/app.js
+++ JS Front End/HTTP and REST/Lab/04. GitHub-Repos/scripts/app.js
@@ -1,3 +1,13 @@
 function loadRepos() {
-   console.log("TODO...");
+   const BASE_URL = 'https://api.github.com/users/testnakov/repos';
+   let result = document.getElementById('res');
+   
+   fetch(BASE_URL, { method: 'GET'})
+   .then((res) => res.text())
+   .then((data) => {
+      result.textContent = data;
+   })
+   .catch((err) => {
+      console.error(err);
+   })
 }
