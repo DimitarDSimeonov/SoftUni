e0486e3f31a9af6273db881b5b308b5a75e171ef
Dimitar Simeonov <simeonov88@abv.bg>
2023-04-02 23:36:33 +0300

    Homework

--- JS Front End/HTTP and REST/Exercise/02.Bus-Schedule/app.js
+++ JS Front End/HTTP and REST/Exercise/02.Bus-Schedule/app.js
@@ -1,9 +1,31 @@
 function solve() {
+    let BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/';
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
+    let spanInfo = document.querySelector('span');
+
+    let nextStop = 'depot';
+    let stopName = '';
     function depart() {
+        fetch(`${BASE_URL}${nextStop}`)
+        .then((res) => res.json())
+        .then((stop) => {
+            stopName = stop.name;
+            spanInfo.textContent = `Next stop ${stopName}`
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+            nextStop = stop.next;
+        })
+        .catch((err) => {
+            console.error(err);
+        });
         // TODO: 
     }
 
     async function arrive() {
+        spanInfo.textContent = `Arriving at ${stopName}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
         // TODO:
     }
 
