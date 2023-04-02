7794a8f4a7fb09560d031a8468979c58d1fa261e
Dimitar Simeonov <simeonov88@abv.bg>
2023-04-02 21:19:57 +0300

    Homework

--- JS Front End/HTTP and REST/Exercise/01.Bus-Stop/app.js
+++ JS Front End/HTTP and REST/Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,23 @@
 function getInfo() {
+    let BASE_URL = 'http://localhost:3030/jsonstore/bus/businfo/';
+    let stopIdInput = document.getElementById('stopId');
+    let stopName = document.getElementById('stopName');
+    let busesUl = document.getElementById('buses');
+
+    let stopId = stopIdInput.value;
+
+    fetch(`${BASE_URL}${stopId}`)
+    .then((res) => res.json())
+    .then((currentStop) => {
+        stopName.textContent = currentStop.name;
+        for (const bus in currentStop.buses) {
+            let li = document.createElement('li');
+            li.textContent = `Bus ${bus} arrives in ${currentStop.buses[bus]} minutes`;
+            busesUl.appendChild(li);
+        }
+    })
+    .catch((err) => {
+        stopName.textContent = 'Error'
+    });
     // TODO:
 }
