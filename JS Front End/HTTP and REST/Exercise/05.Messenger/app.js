caa87924312cb9c2e3ddefd06d8e24a0779956ae
Dimitar Simeonov <simeonov88@abv.bg>
2023-04-04 00:43:56 +0300

    Homework

--- JS Front End/HTTP and REST/Exercise/05.Messenger/app.js
+++ JS Front End/HTTP and REST/Exercise/05.Messenger/app.js
@@ -1,5 +1,46 @@
 function attachEvents() {
-    // TODO:
+    let BASE_URL = 'http://localhost:3030/jsonstore/messenger';
+
+    let textArea = document.getElementById('messages');
+    let authorInput = document.querySelectorAll('input')[0];
+    let messageInput = document.querySelectorAll('input')[1];
+
+    let sendBtn = document.getElementById('submit');
+    let refreshBtn = document.getElementById('refresh');
+
+    refreshBtn.addEventListener('click', refresh);
+    sendBtn.addEventListener('click', send);
+
+    function refresh() {
+        textArea.textContent = '';
+        fetch(BASE_URL)
+        .then((res) => res.json())
+        .then((allMsg) => {
+            let messages  = Object.values(allMsg);
+            let msgArr = []
+            messages.forEach((m) =>{
+                msgArr.push(`${m.author}: ${m.content}`);
+            })
+
+            textArea.textContent = msgArr.join('\n');
+        })
+    }
+
+    function send() {
+        let author = authorInput.value;
+        let content = messageInput.value;
+        fetch(BASE_URL, {
+            method: 'post',
+            body: JSON.stringify({ author, content})
+        })
+        .then(() =>{
+            authorInput.value = '';
+            messageInput.value = '';
+        })
+        .catch((err) =>{
+            console.error(err);
+        })
+    }
 }
 
 attachEvents();
