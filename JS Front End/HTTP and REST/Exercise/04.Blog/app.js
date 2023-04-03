c59452c9d234d07324844a3d879764cb556fdf7c
Dimitar Simeonov <simeonov88@abv.bg>
2023-04-03 23:50:11 +0300

    Homework

--- JS Front End/HTTP and REST/Exercise/04.Blog/app.js
+++ JS Front End/HTTP and REST/Exercise/04.Blog/app.js
@@ -1,5 +1,65 @@
 function attachEvents() {
-    // TODO:
+    let LOAD_URL = 'http://localhost:3030/jsonstore/blog/posts';
+    let COMMENTS_URL = 'http://localhost:3030/jsonstore/blog/comments';
+    let loadBtn = document.getElementById('btnLoadPosts');
+    let select = document.getElementById('posts');
+    let viewBtn = document.getElementById('btnViewPost');
+    let postTitle = document.getElementById('post-title');
+    let postBody = document.getElementById('post-body');
+    let commentsList = document.getElementById('post-comments');
+
+    loadBtn.addEventListener('click', loadPosts);
+    viewBtn.addEventListener('click', loadComments);
+
+    // commentsList.addEventListener('change', () => {
+    //     postTitle.textContent = 'Post Details'
+    //     postBody.textContent = ''
+    // });
+
+    let posts = [];
+
+    function loadPosts() {
+        
+        fetch(LOAD_URL)
+        .then((res) => res.json())
+        .then((allPost) => {
+            posts = Object.values(allPost);
+                
+            posts.forEach((p) => {
+                let option = document.createElement('option');
+                option.value = p.id;
+                option.textContent = p.title;
+                select.appendChild(option); 
+            }) 
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+    }
+
+
+    function loadComments() {
+        commentsList.innerHTML = '';
+        fetch(COMMENTS_URL)
+        .then((res) => res.json())
+        .then((data) => {
+            let comments = Object.values(data);
+
+            let selectedPost = select.options[select.selectedIndex].textContent;
+            let post = posts.find((p) => p.title === selectedPost);
+            console.log(post);
+            postTitle.textContent = post.title;
+            postBody.textContent = post.body;
+
+            comments.forEach((c) => {
+                if (c.postId === post.id ) {
+                    let li = document.createElement('li');
+                    li.textContent = c.text;
+                    commentsList.appendChild(li);
+                }
+            })
+        })
+    }
 }
 
 attachEvents();
