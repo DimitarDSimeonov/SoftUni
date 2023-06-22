b058174b2495bde88ffabe7dbd79c3169765cfe9
Dimitar Simeonov <simeonov88@abv.bg>
2023-06-22 16:26:53 +0300

    Update Main.java

--- Spring Data/Introduction To Hibernate Exercise/src/main/java/Main.java
+++ Spring Data/Introduction To Hibernate Exercise/src/main/java/Main.java
@@ -1,14 +1,37 @@
+import entities.*;
+import tasks.ContainsEmployee;
+import tasks.EmployeesFromDepartment;
+import tasks.IncreaseSalaries;
+import tasks.RemoveTowns;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
+            //ТЕСТВАНЕТО НА 6-ТА МОЖЕ ДА СЧУПИ БАЗАТА ЗА РЕШЕНИЕТО НА 7-МА
+
+
 
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("soft_uni");
-
         EntityManager em = factory.createEntityManager();
 
+        em.getTransaction().begin();
+
+        List<Department> departments = em.createQuery("SELECT d.name, MAX(e.salary) AS max_salary FROM Department AS d\n" +
+                        "JOIN Employee AS e ON e.department_id = d.id\n" +
+                        "GROUP BY d.id\n" +
+                        "HAVING max_salary < 30000 OR max_salary > 70000", Department.class).getResultList()
+                        .stream()
+                        .collect(Collectors.toList());
+
+        departments.forEach(d -> d.getEmployees().stream().max((f, s) -> f.getSalary().compareTo(s.getSalary())).get());
+
+        em.getTransaction().commit();
     }
 }
