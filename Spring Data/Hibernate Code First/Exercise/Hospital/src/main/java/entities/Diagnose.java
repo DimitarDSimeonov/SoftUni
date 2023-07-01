f9e38845c8dff7b7b3a178f694f6ad6ea0dd14bc
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-01 16:50:40 +0300

    Create Diagnose.java

---
+++ Spring Data/Hibernate Code First/Exercise/Hospital/src/main/java/entities/Diagnose.java
@@ -0,0 +1,28 @@
+package entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class Diagnose {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "comments")
+    private String comment;
+
+    @ManyToOne
+    private Patient patient;
+}
