eb71a4d1fc5436f55e844ff291416280c4a63a6f
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-01 16:50:20 +0300

    Create Visitation.java

---
+++ Spring Data/Hibernate Code First/Exercise/Hospital/src/main/java/entities/Visitation.java
@@ -0,0 +1,29 @@
+package entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class Visitation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "comments")
+    private String comment;
+
+    @ManyToOne
+    private Patient patient;
+}
