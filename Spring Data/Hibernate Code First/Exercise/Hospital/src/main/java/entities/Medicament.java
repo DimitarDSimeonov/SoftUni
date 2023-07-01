6f851529b3243f6c3d04780a3637848599acdce3
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-01 16:50:36 +0300

    Create Medicament.java

---
+++ Spring Data/Hibernate Code First/Exercise/Hospital/src/main/java/entities/Medicament.java
@@ -0,0 +1,25 @@
+package entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class Medicament {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    private Patient patient;
+}
