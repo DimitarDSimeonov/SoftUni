cc911fc09392db9627b86c55f9d862f650d33614
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-02 20:53:01 +0300

    Create Main.java

---
+++ Spring Data/Hibernate Code First/Exercise/Gringotts/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("gringotts");
+        EntityManager em = factory.createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.getTransaction().commit();
+    }
+}
