c1923848b7b72f6ed94414fbf9b4121f1d1c8bad
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-02 23:49:23 +0300

    Lab
    Startting spring project

---
+++ Spring Data/Spring Data Intro/Lab/lab/src/main/java/com/lab/entities/Account.java
@@ -0,0 +1,48 @@
+package com.lab.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private BigDecimal balance;
+
+    @ManyToOne
+    private User user;
+
+    public Account() {
+        balance = BigDecimal.ZERO;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ Spring Data/Spring Data Intro/Lab/lab/src/main/java/com/lab/entities/User.java
@@ -0,0 +1,57 @@
+package com.lab.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column
+    private Integer age;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Account> accounts;
+
+    public User() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+}
