5a24807b33635c1f147ca52911664f125d36fe1b
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-03 22:35:47 +0300

    Create CompetitionType.java

---
+++ Spring Data/Hibernate Code First/Exercise/FootballBetting/src/main/java/entities/CompetitionType.java
@@ -0,0 +1,21 @@
+package entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+public class CompetitionType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private  String  name;
+}
