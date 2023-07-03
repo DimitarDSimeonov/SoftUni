759766ad1d1f2b1062b1278e6a342475d6662fd3
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-03 22:35:16 +0300

    Create ResultPrediction.java

---
+++ Spring Data/Hibernate Code First/Exercise/FootballBetting/src/main/java/entities/ResultPrediction.java
@@ -0,0 +1,22 @@
+package entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+public class ResultPrediction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String prediction;
+}
