500bb9ba846ba1fa684e94302c658c68d5f4ffde
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-05 17:00:59 +0300

    Create AccountRepository.java

---
+++ Spring Data/Spring Data Intro/Lab/lab/src/main/java/com/lab/repositories/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.lab.repositories;
+
+import com.lab.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findById(Long id);
+}
