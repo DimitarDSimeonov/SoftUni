2f3bb7becac0b412f5bdea121a9b57a866684169
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-05 17:00:54 +0300

    Create AccountService.java

---
+++ Spring Data/Spring Data Intro/Lab/lab/src/main/java/com/lab/services/AccountService.java
@@ -0,0 +1,9 @@
+package com.lab.services;
+
+import java.math.BigDecimal;
+
+public interface AccountService {
+
+    void withdrawMoney(BigDecimal money, Long id);
+    void depositMoney(BigDecimal money, Long id);
+}
