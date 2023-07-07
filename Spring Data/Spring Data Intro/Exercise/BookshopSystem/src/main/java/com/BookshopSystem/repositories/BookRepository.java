af7a751c2e4a914dfb522a325cf2db7ead981bd4
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-08 00:24:01 +0300

    Update BookRepository.java

--- Spring Data/Spring Data Intro/Exercise/BookshopSystem/src/main/java/com/BookshopSystem/repositories/BookRepository.java
+++ Spring Data/Spring Data Intro/Exercise/BookshopSystem/src/main/java/com/BookshopSystem/repositories/BookRepository.java
@@ -11,4 +11,8 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findAllByReleaseDateAfter(LocalDate localDate);
+
+    List<Book> findAllByReleaseDateBefore(LocalDate localDate);
+
+    List<Book> findAllByAuthorFirstNameAndAuthorLastNameOrderByReleaseDateDescTitleAsc(String firstName, String lastName);
 }
