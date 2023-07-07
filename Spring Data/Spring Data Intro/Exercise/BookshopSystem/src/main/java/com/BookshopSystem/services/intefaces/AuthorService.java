9982ba08a729911cafc9b741e77207fedbaca370
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-08 00:23:25 +0300

    Update AuthorService.java

--- Spring Data/Spring Data Intro/Exercise/BookshopSystem/src/main/java/com/BookshopSystem/services/intefaces/AuthorService.java
+++ Spring Data/Spring Data Intro/Exercise/BookshopSystem/src/main/java/com/BookshopSystem/services/intefaces/AuthorService.java
@@ -3,10 +3,13 @@ package com.BookshopSystem.services.intefaces;
 import com.BookshopSystem.entities.Author;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface AuthorService {
 
     void seedAuthors() throws IOException;
 
     Author getRandomAuthor();
+
+    List<String> getAllOrderByBookCount();
 }
