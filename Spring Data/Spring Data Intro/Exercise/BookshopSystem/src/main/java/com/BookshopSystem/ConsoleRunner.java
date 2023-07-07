d35f5f69705ea2b118ae17511900c6a7b2cdec37
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-08 00:24:37 +0300

    Update ConsoleRunner.java

--- Spring Data/Spring Data Intro/Exercise/BookshopSystem/src/main/java/com/BookshopSystem/ConsoleRunner.java
+++ Spring Data/Spring Data Intro/Exercise/BookshopSystem/src/main/java/com/BookshopSystem/ConsoleRunner.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 @Component
 public class ConsoleRunner implements CommandLineRunner {
@@ -24,10 +25,31 @@ public class ConsoleRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Hello SPRING");
 
         seedData();
 
+        //Before start place set your user and password in application.properties
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Are we going to test the task?[YES/NO]");
+        String input = scanner.nextLine();
+
+        while (!input.equalsIgnoreCase("NO")) {
+
+            System.out.println("Enter the number of task[1-4]");
+            input = scanner.nextLine();
+
+            switch (input) {
+                case "1" -> taskOne();
+                case "2" -> taskTwo();
+                case "3" -> taskThree();
+                case "4" ->  taskFour();
+            }
+
+            System.out.println("Other task?[YES/NO]");
+            input = scanner.nextLine();
+        }
+        System.out.println("Have a nice day!");
 
     }
 
@@ -41,5 +63,19 @@ public class ConsoleRunner implements CommandLineRunner {
     private void taskOne() {
         bookService.getAllBookAfterYear(LocalDate.of(2000, 12, 31))
                 .forEach(b -> bookService.printBookTitle(b));
+    }
+
+    private void taskTwo() {
+        bookService.getAllBookBeforeYear(1990)
+                .forEach(System.out::println);
+    }
+
+    private void taskThree() {
+        authorService.getAllOrderByBookCount().forEach(System.out::println);
+    }
+
+    private void taskFour() {
+        bookService.getAllByAuthorFirstNameAndAuthorLastNameOrderByReleaseDateDescTitleAsc("George", "Powell")
+                .forEach(System.out::println);
     }
 }
