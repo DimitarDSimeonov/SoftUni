5ce6115ed0df9cd9d9fd94a38e73df234bbafb67
Dimitar Simeonov <simeonov88@abv.bg>
2023-07-07 22:16:53 +0300

    Update BookService.java

--- Spring Data/Spring Data Intro/Exercise/BookshopSystem/src/main/java/com/BookshopSystem/services/intefaces/BookService.java
+++ Spring Data/Spring Data Intro/Exercise/BookshopSystem/src/main/java/com/BookshopSystem/services/intefaces/BookService.java
@@ -1,9 +1,16 @@
 package com.BookshopSystem.services.intefaces;
 
+import com.BookshopSystem.entities.Book;
+
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface BookService {
 
     void seedBook() throws IOException;
 
+    List<Book> getAllBookAfterYear(LocalDate localDate);
+
+    void printBookTitle(Book book);
 }
