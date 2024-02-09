74223a445e91902512d743f16a43e28e88d2b092
Dimitar Simeonov <simeonov88@abv.bg>
2024-02-09 12:36:14 +0200

    Homework

---
+++ Container And Clouds/Infrastructure as code/Exercises/1.Azure Resource Group/azure-rg.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "mitko_rg" {
+  name     = "example-mitko"
+  location = "West Europe"
+}
